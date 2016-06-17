8ec40bde18ef4b328d26b6e931809b0160dfd251
Fernando Bevilacqua <dovyski@gmail.com>
2016-06-17 10:33:53 +0200

    Add a bit of comments

--- example/src/main-app/main.cpp
+++ example/src/main-app/main.cpp
@@ -1,3 +1,10 @@
+/*
+This is a demo application to showcase the UI components of cvui.
+
+Copyright (c) 2016 Fernando Bevilacqua <dovyski@gmail.com>
+Licensed under the MIT license.
+*/
+
 #include <iostream>
 
 #include "opencv2/core/core.hpp"
@@ -10,36 +17,68 @@
 
 int main(int argc, const char *argv[])
 {
-	cv::Mat aFrame = cv::Mat(800, 600, CV_8UC3);
-	bool aChecked = false;
-	int aCount = 0;
+	cv::Mat frame = cv::Mat(250, 600, CV_8UC3);
+	bool checked = false;
+	bool checked2 = true;
+	int count = 0;
 
+	// Init a OpenCV window and tell cvui to use it.
+	// If cv::namedWindow() is not used, mouse events will
+	// not be captured by cvui.
 	cv::namedWindow(WINDOW_NAME);
 	cvui::init(WINDOW_NAME);
 
 	while (true) {
 		// Fill the frame with a nice color
-		aFrame = cv::Scalar(49, 52, 49);
+		frame = cv::Scalar(49, 52, 49);
 
 		// Check if ESC key was pressed
 		if (cv::waitKey(10) == 27) {
 			break;
 		}
 
-		cvui::text(aFrame, 50, 30, "Hey there!", 0.4);
+		// Show some pieces of text. You can also specify the size of the
+		// text and its color (using hex 0xRRGGBB CSS-like style).
+		cvui::text(frame, 50, 30, "Hey there!");
+		cvui::text(frame, 200, 30, "Use hex 0xRRGGBB colors easily", 0.4, 0xff0000);
 
-		if (cvui::button(aFrame, 50, 50, "Button")) {
+		// Buttons will return true if they were clicked, which makes
+		// handling clicks a breeze.
+		if (cvui::button(frame, 50, 50, "Button")) {
 			std::cout << "Button clicked!" << std::endl;
 		}
 
-		cvui::checkbox(aFrame, 50, 100, "Checkbox test", &aChecked);
-		cvui::window(aFrame, 50, 150, 120, 100, "My Window");
-		cvui::counter(aFrame, 50, 280, &aCount);
+		// If you do not specify the button width/height, the size will be
+		// automatically adjusted to properly house the label.
+		cvui::button(frame, 200, 50, "Button with large label");
+		// You can tell the width and height you want
+		cvui::button(frame, 410, 50, 15, 15, "x");
 
+		// Window components are useful to create HUDs and similars. At the
+		// moment, there is no implementation to constraint content within a
+		// a window.
+		cvui::window(frame, 50, 100, 120, 100, "Window");
+		
+		// The counter component can be used to alter int variables. Use
+		// the 4th parameter of the function to point it to the variable
+		// to be changed.
+		cvui::counter(frame, 200, 100, &count);
+		
+		// Checkboxes also accept a pointer to a variable that controls
+		// the state of the checkbox (checked or not). cvui::checkbox() will
+		// automatically update the value of the boolean after all
+		// interactions, but you can also change it by yourself. Just
+		// do "checked = true" somewhere and the checkbox will change
+		// its appearance.
+		cvui::checkbox(frame, 200, 150, "Checkbox", &checked);
+		cvui::checkbox(frame, 200, 180, "A checked checkbox", &checked2);
+
+		// This function must be called *AFTER* all UI components. It does
+		// all the behind the scenes magic to handle mouse clicks, etc.
 		cvui::update();
 
 		// Show everything on the screen
-		imshow(WINDOW_NAME, aFrame);
+		cv::imshow(WINDOW_NAME, frame);
 	}
 
 	return 0;
