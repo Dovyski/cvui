0a6021c26907c29ac53ce8a31903e20eca7f4266
Pascal Thomet <pthomet@ivsweb.com>
2016-09-10 04:26:14 +0200

    add animated sparkline in example app, just for fun

--- example/src/main-app/main.cpp
+++ example/src/main-app/main.cpp
@@ -16,7 +16,7 @@ Licensed under the MIT license.
 
 int main(int argc, const char *argv[])
 {
-	cv::Mat frame = cv::Mat(310, 600, CV_8UC3);
+	cv::Mat frame = cv::Mat(400, 600, CV_8UC3);
 	bool checked = false;
 	bool checked2 = true;
 	int count = 0;
@@ -100,6 +100,7 @@ int main(int argc, const char *argv[])
 		//bool DrawValuesAtLargeSteps : draw value at large steps
 		//std::string Printf_Format : printf format string of the values and legend
 		static double value = 2.25;
+		static std::vector<double> lastValues;
 		cvui::TrackbarParams params;
 		params.MinimumValue = 0.;
 		params.MaximumValue = 5;
@@ -109,8 +110,15 @@ int main(int argc, const char *argv[])
 		params.ForceValuesAsMultiplesOfSmallStep = true;
 		if (cvui::trackbar(frame, 200, 230, &value, params)) {
 			std::cout << "Tracbar was modified, value : " << value << std::endl;
+			lastValues.push_back(value);
 		}
 
+		if (lastValues.size() > 5) {
+			cvui::text(frame, 200, 285, "<- Your edits on a sparkline");
+			cvui::sparkline(frame, lastValues, 150, 289, 40, 15);
+			if (cvui::button(frame, 200, 315, "Clear sparkline"))
+				lastValues.clear();
+		}
 
 		// Display the lib version at the bottom of the screen
 		cvui::printf(frame, frame.cols - 80, frame.rows - 20, 0.4, 0xCECECE, "cvui v.%s", cvui::VERSION);
