f44183f0472dd510f6cbf947daa816da49b1560f
Fernando Bevilacqua <dovyski@gmail.com>
2017-04-28 11:26:29 +0200

    Bump version to 2.0.0

--- cvui.h
+++ cvui.h
@@ -1,7 +1,7 @@
 /*
  A (very) simple UI lib built on top of OpenCV drawing primitives.
 
- Version: 1.1.0
+ Version: 2.0.0
 
  Copyright (c) 2016 Fernando Bevilacqua <dovyski@gmail.com>
  Licensed under the MIT license.
@@ -806,7 +806,7 @@ void handleMouse(int theEvent, int theX, int theY, int theFlags, void* theData);
 #endif
 
 // Lib version
-static const char *VERSION = "1.1.0";
+static const char *VERSION = "2.0.0";
 
 const int ROW = 0;
 const int COLUMN = 1;
