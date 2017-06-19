58d1be2bf006cbf06bac15412ad418fd1d92f32b
Fernando Bevilacqua <dovyski@gmail.com>
2017-06-19 15:06:51 +0200

    Add opencv 2.4.13.2 and 3.2.0 to CI testing matrix

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,10 +6,10 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 	sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 	sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
-	curl -sL https://github.com/Itseez/opencv/archive/$OPENCV_VERSION.zip > opencv.zip
+	curl -sL https://github.com/Itseez/opencv/archive/$OPENCV.zip > opencv.zip
 	unzip opencv.zip
 
-	cd opencv-$OPENCV_VERSION
+	cd opencv-$OPENCV
 
 	# Create a new 'build' folder.
 	mkdir build
