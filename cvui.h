9c0d6c702519d8a6bddaf806371ec664a6fb93bd
Fernando Bevilacqua <dovyski@gmail.com>
2018-04-04 10:54:15 +0200

    Drop BETA label

--- cvui.h
+++ cvui.h
@@ -1,7 +1,7 @@
 /*
  A (very) simple UI lib built on top of OpenCV drawing primitives.
 
- Version: 2.5.0-BETA
+ Version: 2.5.0
 
  Copyright (c) 2016 Fernando Bevilacqua <dovyski@gmail.com>
  Licensed under the MIT license.
@@ -1032,7 +1032,7 @@ void handleMouse(int theEvent, int theX, int theY, int theFlags, void* theData);
 #endif
 
 // Lib version
-static const char *VERSION = "2.5.0-BETA";
+static const char *VERSION = "2.5.0";
 
 const int ROW = 0;
 const int COLUMN = 1;
