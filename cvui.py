26bc602587329b106ef895288cca3da679792198
Fernando Bevilacqua <dovyski@gmail.com>
2018-05-25 14:29:11 +0200

    Python: initial work to mimic internal cvui structure

--- cvui.py
+++ cvui.py
@@ -9,7 +9,139 @@ def main():
 if __name__ == '__main__':
     main()
 
-def random_number_generator(arg1, arg2):
+RIGHT_BUTTON = 0
+MIDDLE_BUTTON = 1
+LEFT_BUTTON = 2
+
+# Class to represent 2D points
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# Describes the block structure used by the lib to handle `begin*()` and `end*()` calls.
+class Block:
+    __slots__ = [
+        'where',       # where the block should be rendered to.
+        'rect',        # the size and position of the block.
+        'fill',        # the filled area occuppied by the block as it gets modified by its inner components.
+        'anchor',      # the point where the next component of the block should be rendered.
+        'padding',     # padding among components within this block.
+        'type'         # type of the block, e.g. ROW or COLUMN.
+    ]
+
+# Describes a component label, including info about a shortcut.
+# If a label contains "Re&start", then:
+# - hasShortcut will be true
+# - shortcut will be 's'
+# - textBeforeShortcut will be "Re"
+# - textAfterShortcut will be "tart"
+class Label:
+    __slots__ = [
+        'hasShortcut',
+        'shortcut',
+        'textBeforeShortcut',
+        'textAfterShortcut'
+    ]
+
+# Describe a mouse button
+class MouseButton:
+    __slots__ = [
+        'justReleased',  # if the mouse button was released, i.e. click event.
+        'justPressed',   # if the mouse button was just pressed, i.e. true for a frame when a button is down.
+        'pressed'        # if the mouse button is pressed or not.
+    ]
+
+# Describe the information of the mouse cursor
+class Mouse:
+    buttons = {}             # status of each button. Use cvui::{RIGHT,LEFT,MIDDLE}_BUTTON to access the buttons.
+    anyButton = False        # represent the behavior of all mouse buttons combined
+    position = Point(0, 0)   # x and y coordinates of the mouse at the moment.
+
+# Describes a (window) context.
+class Context:
+	windowName = '',       # name of the window related to this context.
+	mouse = Mouse()        # the mouse cursor related to this context.
+
+# This class contains all stuff that cvui uses internally to render
+# and control interaction with components
+class Internal:
+    defaultContext = ''
+    currentContext = ''
+    contexts = {} # indexed by the window name.
+    buffer = []
+    lastKeyPressed = -1 # TODO: collect it per window
+    delayWaitKey = -1
+    screen = Block()
+    stack = []
+    stackCount = -1
+    trackbarMarginX = 14
+
+    def __init__(self):
+        self.defaultContext = ''
+
+    def init(self, theWindowName, theDelayWaitKey):
+        self.defaultContext = theWindowName
+        self.currentContext = theWindowName
+        self.delayWaitKey = theDelayWaitKey
+        self.lastKeyPressed = -1
+
+# Class that contains all rendering methods.
+class Render:
+    def text(self, theBlock, theText, thePos, theFontScale, theColor):
+        print('not implemented yet')
+
+# Access point to the internal namespace.
+__internal = Internal()
+__render = Render()
+
+def _handleMouse(theEvent, theX, theY, theFlags, theData):
+	#int aButtons[3] = { cvui::LEFT_BUTTON, cvui::MIDDLE_BUTTON, cvui::RIGHT_BUTTON };
+	#int aEventsDown[3] = { cv::EVENT_LBUTTONDOWN, cv::EVENT_MBUTTONDOWN, cv::EVENT_RBUTTONDOWN };
+	#int aEventsUp[3] = { cv::EVENT_LBUTTONUP, cv::EVENT_MBUTTONUP, cv::EVENT_RBUTTONUP };
+	
+	#cvui_context_t *aContext = (cvui_context_t *)theData;
+	
+	#for (int i = 0; i < 3; i++) {
+	#	int aBtn = aButtons[i];
+
+	#	if (theEvent == aEventsDown[i]) {
+	#		aContext->mouse.anyButton.justPressed = true;
+	#		aContext->mouse.anyButton.pressed = true;
+	#		aContext->mouse.buttons[aBtn].justPressed = true;
+	#		aContext->mouse.buttons[aBtn].pressed = true;
+
+	#	} else if (theEvent == aEventsUp[i]) {
+	#		aContext->mouse.anyButton.justReleased = true;
+	#		aContext->mouse.anyButton.pressed = false;
+	#		aContext->mouse.buttons[aBtn].justReleased = true;
+	#		aContext->mouse.buttons[aBtn].pressed = false;
+	#	}
+	#}
+	
+	#aContext->mouse.position.x = theX;
+	#aContext->mouse.position.y = theY;
+    print('_handleMouse', theEvent, theX, theY, theFlags, theData)
+
+def watch(theWindowName, theDelayWaitKey = -1, theCreateNamedWindow = True):
+    aContex = Context()
+
+    if theCreateNamedWindow:
+        cv2.namedWindow(theWindowName)
+
+    aContex.windowName = theWindowName
+    aContex.mouse.position.x = 0
+    aContex.mouse.position.y = 0
+
+    #__internal.resetMouseButton(aContex.mouse.anyButton)
+    #__internal.resetMouseButton(aContex.mouse.buttons[RIGHT_BUTTON])
+    #__internal.resetMouseButton(aContex.mouse.buttons[MIDDLE_BUTTON])
+    #__internal.resetMouseButton(aContex.mouse.buttons[LEFT_BUTTON])
+
+    __internal.contexts[theWindowName] = aContex
+    cv2.setMouseCallback(theWindowName, _handleMouse, __internal.contexts[theWindowName])
+
+def init(theWindowName, theDelayWaitKey = -1, theCreateNamedWindow = True):
     """
     Summary line.
 
@@ -28,10 +160,8 @@ def random_number_generator(arg1, arg2):
         Description of return value
 
     """
-    return 42
-
-def init(window_name, delay_wait_key = -1, create_named_window = True):
-	cv2.namedWindow(window_name)
+    __internal.init(theWindowName, theDelayWaitKey)
+    watch(theWindowName, theCreateNamedWindow)
 
 def text(where, x, y, text, font_scale = 0.4, color = 0xCECECE):
 	cv2.putText(where, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 255, 255), 1, cv2.LINE_AA)
