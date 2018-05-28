3cd92665a3304458aaa24429297e7a26a594a160
Fernando Bevilacqua <dovyski@gmail.com>
2018-05-28 10:08:05 +0200

    Python: draft port of the mouse API
    The mouse example is not fully working, but it's a beginning nevertheless.

--- cvui.py
+++ cvui.py
@@ -131,6 +131,18 @@ class Internal:
 		self.stackCount = -1
 		self.trackbarMarginX = 14
 
+	def isMouseButton(self, theButton, theQuery):
+		aRet = False
+
+		if theQuery == CLICK or theQuery == UP:
+			aRet = theButton.justReleased
+		elif theQuery == DOWN:
+			aRet = theButton.justPressed
+		elif theQuery == IS_DOWN:
+			aRet = theButton.pressed
+
+		return aRet
+
 	def init(self, theWindowName, theDelayWaitKey):
 		self.defaultContext = theWindowName
 		self.currentContext = theWindowName
@@ -403,6 +415,98 @@ def printf(theWhere, theX, theY, theFmt, *theArgs):
 def checkbox(theWhere, theX, theY, theLabel, theState, theColor = 0xCECECE):
 	__internal.screen.where = theWhere
 	return __internal.checkbox(__internal.screen, theX, theY, theLabel, theState, theColor)
+
+def mouse(theWindowName = ''):
+	"""
+	 Return the last position of the mouse.
+
+	 \param theWindowName name of the window whose mouse cursor will be used. If nothing is informed (default), the function will return the position of the mouse cursor for the default window (the one informed in `cvui::init()`).
+	 \return a point containing the position of the mouse cursor in the speficied window.
+	"""
+	return __internal.getContext(theWindowName).mouse.position
+
+def mouse2(theQuery):
+	"""
+	Query the mouse for events, e.g. "is any button down now?". Available queries are:
+ 
+	* `cvui::DOWN`: any mouse button was pressed. `cvui::mouse()` returns `true` for a single frame only.
+	* `cvui::UP`: any mouse button was released.  `cvui::mouse()` returns `true` for a single frame only.
+	* `cvui::CLICK`: any mouse button was clicked (went down then up, no matter the amount of frames in between). `cvui::mouse()` returns `true` for a single frame only.
+	* `cvui::IS_DOWN`: any mouse button is currently pressed. `cvui::mouse()` returns `true` for as long as the button is down/pressed.
+
+	It is easier to think of this function as the answer to a questions. For instance, asking if any mouse button went down:
+
+	```
+	if (cvui::mouse(cvui::DOWN)) {
+	  // Any mouse button just went down.
+	}
+	```
+
+	The window whose mouse will be queried depends on the context. If `cvui::mouse(query)` is being called after
+	`cvui::context()`, the window informed in the context will be queried. If no context is available, the default
+	window (informed in `cvui::init()`) will be used.
+
+	Parameters
+    ----------
+	theQuery: int
+		Integer describing the intended mouse query. Available queries are `cvui::DOWN`, `cvui::UP`, `cvui::CLICK`, and `cvui::IS_DOWN`.
+
+	\sa mouse(const cv::String&)
+	\sa mouse(const cv::String&, int)
+	\sa mouse(const cv::String&, int, int)
+	\sa mouse(int, int)
+	"""
+	return mouse3('', theQuery)
+
+def mouse3(theWindowName, theQuery):
+	"""
+	 Query the mouse for events in a particular window. This function behave exactly like `cvui::mouse(int theQuery)`
+	 with the difference that queries are targeted at a particular window.
+
+	 \param theWindowName name of the window that will be queried.
+	 \param theQuery an integer describing the intended mouse query. Available queries are `cvui::DOWN`, `cvui::UP`, `cvui::CLICK`, and `cvui::IS_DOWN`.
+
+	 \sa mouse(const cv::String&)
+	 \sa mouse(const cv::String&, int, int)
+	 \sa mouse(int, int)
+	 \sa mouse(int)
+	"""
+	aButton = __internal.getContext(theWindowName).mouse.anyButton
+	aRet = __internal.isMouseButton(aButton, theQuery)
+
+	return aRet
+
+def mouse4(theButton, theQuery):
+	"""
+	 Query the mouse for events in a particular button. This function behave exactly like `cvui::mouse(int theQuery)`,
+	 with the difference that queries are targeted at a particular mouse button instead.
+
+	 \param theButton an integer describing the mouse button to be queried. Possible values are `cvui::LEFT_BUTTON`, `cvui::MIDDLE_BUTTON` and `cvui::LEFT_BUTTON`.
+	 \param theQuery an integer describing the intended mouse query. Available queries are `cvui::DOWN`, `cvui::UP`, `cvui::CLICK`, and `cvui::IS_DOWN`.
+
+	 \sa mouse(const cv::String&)
+	 \sa mouse(const cv::String&, int, int)
+	 \sa mouse(int)
+	"""
+	return mouse5('', theButton, theQuery)
+
+def mouse5(theWindowName, theButton, theQuery):
+	"""
+	 Query the mouse for events in a particular button in a particular window. This function behave exactly
+	 like `cvui::mouse(int theButton, int theQuery)`, with the difference that queries are targeted at
+	 a particular mouse button in a particular window instead.
+
+	 \param theWindowName name of the window that will be queried.
+	 \param theButton an integer describing the mouse button to be queried. Possible values are `cvui::LEFT_BUTTON`, `cvui::MIDDLE_BUTTON` and `cvui::LEFT_BUTTON`.
+	 \param theQuery an integer describing the intended mouse query. Available queries are `cvui::DOWN`, `cvui::UP`, `cvui::CLICK`, and `cvui::IS_DOWN`.
+	"""
+	if theButton != RIGHT_BUTTON and theButton != MIDDLE_BUTTON and theButton != LEFT_BUTTON:
+		__internal.error(6, 'Invalid mouse button. Are you using one of the available: cvui.{RIGHT,MIDDLE,LEFT}_BUTTON ?')
+
+	aButton = __internal.getContext(theWindowName).mouse.buttons[theButton]
+	aRet = __internal.isMouseButton(aButton, theQuery)
+
+	return aRet
 
 def button(where, x, y, label):
 	# Not implemented yet!
---
+++ example/src/mouse/mouse.py
@@ -0,0 +1,86 @@
+#
+# This is application shows the mouse API of cvui. It shows a rectangle on the
+# screen everytime the user clicks and drags the mouse cursor around.
+#
+# Copyright (c) 2018 Fernando Bevilacqua <dovyski@gmail.com>
+# Licensed under the MIT license.
+#
+
+# Include the directory where cvui is so we can load it
+import sys
+sys.path.append('../../../')
+
+import numpy as np
+import cv2
+import cvui
+
+WINDOW_NAME	= 'Mouse'
+
+def main():
+	frame = np.zeros((300, 600, 3), np.uint8)
+
+	# Init cvui and tell it to create a OpenCV window, i.e. cv::namedWindow(WINDOW_NAME).
+	cvui.init(WINDOW_NAME);
+
+	# Rectangle to be rendered according to mouse interactions.
+	rectangle = cvui.Rect(0, 0, 0, 0)
+
+	while (True):
+		# Fill the frame with a nice color
+		frame[:] = (49, 52, 49)
+
+		# Show the coordinates of the mouse pointer on the screen
+		cvui.text(frame, 10, 30, 'Click (any) mouse button and drag the pointer around to select an area.')
+		cvui.printf(frame, 10, 50, 'Mouse pointer is at (%d,%d)', cvui.mouse().x, cvui.mouse().y)
+
+		# The function "bool cvui.mouse(int query)" allows you to query the mouse for events.
+		# E.g. cvui.mouse(cvui.DOWN)
+		#
+		# Available queries:
+		#	- cvui.DOWN: any mouse button was pressed. cvui.mouse() returns true for a single frame only.
+		#	- cvui.UP: any mouse button was released. cvui.mouse() returns true for a single frame only.
+		#	- cvui.CLICK: any mouse button was clicked (went down then up, no matter the amount of frames in between). cvui.mouse() returns true for a single frame only.
+		#	- cvui.IS_DOWN: any mouse button is currently pressed. cvui.mouse() returns true for as long as the button is down/pressed.
+
+		# Did any mouse button go down?
+		if cvui.mouse2(cvui.DOWN):
+			# Position the rectangle at the mouse pointer.
+			rectangle.x = cvui.mouse().x
+			rectangle.y = cvui.mouse().y
+
+		# Is any mouse button down (pressed)?
+		if cvui.mouse2(cvui.IS_DOWN):
+			# Adjust rectangle dimensions according to mouse pointer
+			rectangle.width = cvui.mouse().x - rectangle.x
+			rectangle.height = cvui.mouse().y - rectangle.y
+
+			# Show the rectangle coordinates and size
+			cvui.printf(frame, rectangle.x + 5, rectangle.y + 5, 0.3, 0xff0000, '(%d,%d)', rectangle.x, rectangle.y)
+			cvui.printf(frame, cvui.mouse().x + 5, cvui.mouse().y + 5, 0.3, 0xff0000, 'w:%d, h:%d', rectangle.width, rectangle.height)
+
+		# Did any mouse button go up?
+		if cvui.mouse2(cvui.UP):
+			# Hide the rectangle
+			rectangle.x = 0
+			rectangle.y = 0
+			rectangle.width = 0
+			rectangle.height = 0
+
+		# Was the mouse clicked (any button went down then up)?
+		if cvui.mouse2(cvui.CLICK):
+			cvui.text(frame, 10, 70, 'Mouse was clicked!')
+
+		# Render the rectangle
+		cvui.rect(frame, rectangle.x, rectangle.y, rectangle.width, rectangle.height, 0xff0000)
+
+		# This function must be called *AFTER* all UI components. It does
+		# all the behind the scenes magic to handle mouse clicks, etc, then
+		# shows the frame in a window like cv2.imshow() does.
+		cvui.imshow(WINDOW_NAME, frame)
+
+		# Check if ESC key was pressed
+		if cv2.waitKey(20) == 27:
+			break
+
+if __name__ == '__main__':
+	main()
